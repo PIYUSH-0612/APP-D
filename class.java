e16722bbaca09d060d3e3a966fcd6ae092a2b2b0
Piyush Gupta <122008271+PIYUSH-0612@users.noreply.github.com>
2023-06-20 13:56:57 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,20 @@
+class all_adata{
+    int a;
+    String b;
+    float c;
+    char d;
+}
+class check
+{
+    public static void main(String[]arg){
+       all_adata A= new all_adata();
+       A.a=10;
+       A.b="hello";
+       A.c=7.85f;
+       System.out.print(A.a+A.b+A.c);
+    }
+}
+
+
+        
+       
